faa79cffccc79169763ebab5b44b7c008e122da9
Martin Danielyan <mr.danielyan55@yahoo.com>
2018-09-24 10:43:55 +0300

    Lest fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 /**
  * @param someone
- * @return Haloxaxalxalxalxal 
+ * @return greeting string 
  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This is package file
+ * This is package name
  */
 package com.example;
 
